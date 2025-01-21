a191a2726a259e590bd33384618d5442eb9d22a5
yxxndxxn <yxxndxxn@gmail.com>
2025-01-21 23:47:01 +0900

    ✨ feat: 로딩중 설정 및 firebase 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
 
 const router = createBrowserRouter([
   {
@@ -45,9 +48,20 @@ font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, O
 }
 `;
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const init = async () => {
+    await auth.authStateReady(); //최초 인증 상태가 완료될 때 실행되는 Promise를 return/ 로그인 여부를 확인하는 동안 기다리겠따는 뜻..
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <>
       <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
       <RouterProvider router={router} />
     </>
   );
---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCAhVg7wN3jWGUkegm2Zj8tH_c1fT2UdE4",
+  authDomain: "nwitter-reloaded-34ebe.firebaseapp.com",
+  projectId: "nwitter-reloaded-34ebe",
+  storageBucket: "nwitter-reloaded-34ebe.firebasestorage.app",
+  messagingSenderId: "845391556823",
+  appId: "1:845391556823:web:d4d615acc1a03051a08092",
+  measurementId: "G-ZP261X56ZP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
