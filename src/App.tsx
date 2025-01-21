ef4459d7226129afef78e14013dc762bb1b658b6
yxxndxxn <yxxndxxn@gmail.com>
2025-01-21 17:44:09 +0900

    ✨ feat: Routing 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,56 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  //login, create-account는 Layout으로 감싸지 않기 위해서 밖으로 뺀겨
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+//디폴트 스타일 설정(검은배경, 흰 글씨...)
+const GlobalStyles = createGlobalStyle`${reset};
+*{
+  box-sizing: border-box;
+}
+body{
+background-color: black;
+color:white;
+font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`;
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
