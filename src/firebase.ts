7524a5bd61089498e963ad1b2aa5808a1c898823
yxxndxxn <yxxndxxn@gmail.com>
2025-01-22 11:43:44 +0900

    ✨ feat: firebase 유저 계정 생성 구현

--- src/firebase.ts
+++ src/firebase.ts
@@ -20,4 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const auth = getAuth(app);
+export const auth = getAuth(app); //인증 인스턴스
--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,8 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import styled from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   width: 420px;
@@ -39,18 +42,20 @@ const Error = styled.span`
   color: #ed4848;
 `;
 export default function CreateAccount() {
-  const [Loading, setLoading] = useState(false); //계정 생성할 때 true
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false); //계정 생성할 때 true
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  //성별, 나이, 학교, 흡연 여부, 연락처(카카오톡 ID...?)
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { name, value },
     } = e;
     //모든 input에 name을 넣은 이유..
     //요렇게 하면 input이 변경되었을 때, 어떤 input이 변경되었는지 찾을 수 있음!
-    if (name === "name") {
+    if (loading || name === "name") {
       setName(value);
     } else if (name === "email") {
       setEmail(value);
@@ -59,12 +64,28 @@ export default function CreateAccount() {
     }
   };
 
-  const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(name, email, password);
+    if (name === "" || email === "" || password === "") return;
     try {
+      setLoading(true);
+      //가입한 유저 정보
       //계정 만들기
+      const credentials = await createUserWithEmailAndPassword(
+        //createUserWithEmailAndPassword특징: 계정이 생성되면 자동으로 로그인 됨!
+        auth,
+        email,
+        password //최소 6글자 넘어야함.. 안 그러면 POST 400에러..
+      );
+      console.log(credentials.user);
+
       //사용자의 이름 설정
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      navigate("/"); //계정 생성, 사용자 프로필 업데이트 후 홈 이동
+
       //홈 페이지로 이동
     } catch (e) {
       //setError
@@ -74,7 +95,7 @@ export default function CreateAccount() {
   };
   return (
     <Wrapper>
-      <Title>로그인</Title>
+      <Title>join</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
@@ -100,7 +121,7 @@ export default function CreateAccount() {
           type="password"
           required
         />
-        <Input type="submit" value={Loading ? "Loading" : "Create Account"} />
+        <Input type="submit" value={loading ? "Loading" : "Create Account"} />
       </Form>
       {error !== "" ? <Error>{error}</Error> : null}
     </Wrapper>
