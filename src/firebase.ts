0a03269b9135dfd8737912fa762040423a340f49
yxxndxxn <yxxndxxn@gmail.com>
2025-01-26 23:24:24 +0900

    🔧 fix: env 설정

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,16 +6,22 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const apiKey = process.env.REACT_APP_API_KEY;
+const authDomain = process.env.REACT_APP_AUTH_DOMAIN;
+const projectId = process.env.REACT_APP_PROJECT_ID;
+const storageBucket = process.env.REACT_APP_STORAGE_BUCKET;
+const messagingSenderId = process.env.REACT_APP_MESSAGEING_SENDER_ID;
+const appId = process.env.REACT_APP_APP_ID;
+const measurementId = process.env.REACT_APP_MEASUREMENT_ID;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC94g6P_QT0lZNV40vqJIG-hEGppLqe5bE",
-  authDomain: "roommate-65208.firebaseapp.com",
-  projectId: "roommate-65208",
-  storageBucket: "roommate-65208.firebasestorage.app",
-  messagingSenderId: "632142580032",
-  appId: "1:632142580032:web:b228940dba0ae64609d2a5",
-  measurementId: "G-G6F6X03YJX",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 };
 
 // Initialize Firebase
